0bcf15f19050bff0ef8d72c0e18157d18af1b564
Larry Lopez <154121+namralkeeg@users.noreply.github.com>
2018-10-05 13:16:52 -0700

    Added Selection Sort algorithm and tests.

---
+++ src/Kettle/Sorting/SelectionSort.cs
@@ -0,0 +1,79 @@
+﻿#region Copyright
+
+/*
+ * Copyright (C) 2018 Larry Lopez
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#endregion Copyright
+
+using System;
+using System.Collections.Generic;
+using Kettle.Common;
+
+namespace Kettle.Sorting
+{
+    /// <summary>
+    /// A Selection Sort implementation of the <see cref="ICompareSort{T}"/> interface.
+    /// </summary>
+    /// <typeparam name="T">The type of objects to sort. Must implement <see cref="IComparable{T}"/>.</typeparam>
+    public sealed class SelectionSort<T> : CompareSort<T> where T : IComparable<T>
+    {
+        /// <summary>
+        /// Initializes a <see cref="SelectionSort{T}"/> class.
+        /// </summary>
+        public SelectionSort()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a <see cref="SelectionSort{T}"/> class.
+        /// </summary>
+        /// <param name="comparer">The <see cref="IComparer{T}"/> to use for all sorting comparisons.</param>
+        public SelectionSort(IComparer<T> comparer) : base(comparer)
+        {
+        }
+
+        /// <inheritdoc/>
+        public override void Sort(IList<T> list, int start, int count)
+        {
+            SortValidationCheck(list, start, count);
+
+            int least;
+            int end = start + count;
+            for (int i = start; i < end; i++)
+            {
+                least = i;
+                for (int j = i + 1; j < end; j++)
+                {
+                    if (Comparer.Compare(list[j], list[least]) < 0)
+                    {
+                        least = j;
+                    }
+                }
+
+                if (least != i)
+                {
+                    Utils.Swap(list, i, least);
+                }
+            }
+        }
+    }
+}
